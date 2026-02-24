b178d17ee20c1f682daa6800240cd6f5bba8754c
Nathan Glaser <glaserna@oregonstate.edu>
2026-02-24 12:48:03 -0800

    clang format geometry base

--- tests/test_geometry_base.cc
+++ tests/test_geometry_base.cc
@@ -1,8 +1,5 @@
 #include <gtest/gtest.h>
+
 #include "geometry_base.h"
 
-TEST(GeometryBase, BasicConstructor) {
-  EXPECT_NO_THROW(
-    GeometryBase()
-  );
-}
+TEST(GeometryBase, BasicConstructor) { EXPECT_NO_THROW(GeometryBase()); }
