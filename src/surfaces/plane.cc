c028bfdbb68e637dd894d513bd30bb6dccce6191
Nathan Glaser <glaserna@oregonstate.edu>
2026-02-24 16:12:52 -0800

    add source implementation for planes

---
+++ src/surfaces/plane.cc
@@ -0,0 +1,24 @@
+#include "surfaces/plane.h"
+
+#include <cmath>
+
+#include "basic_types.h"
+
+namespace charmander {
+Plane::Plane(double a, double b, double c, double d)
+    : Surface(), a_(a), b_(b), c_(c), d_(d) {};
+
+double Plane::Evaluate(Point p) const {
+  return a_ * p.x + b_ * p.y + c_ * p.z - d_;
+};
+
+Direction Plane::Normal(Point p) const { return {a_, b_, c_}; };
+
+double Plane::Distance(Point p, Direction d) const {
+  double denominator = (d.x * a_ + d.y * b_ + d.z * c_);
+  if (std::abs(denominator) < 1e-15) return INF;
+
+  double distance = -Evaluate(p) / denominator;
+  return (distance > 0) ? distance : INF;
+};
+}  // namespace charmander
