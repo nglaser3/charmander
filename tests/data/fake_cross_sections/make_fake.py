88b1b16f3c16651e3ed91005fc1d0f613527ac4e
Nathan Glaser <glaserna@oregonstate.edu>
2026-03-02 21:28:53 -0800

    add fake xs files for better test isolation

---
+++ tests/data/fake_cross_sections/make_fake.py
@@ -0,0 +1,18 @@
+import h5py
+import numpy as np
+from pathlib import Path
+
+out = Path("BadFakeU235.h5")
+with h5py.File(out, "w") as f:
+    # this file is for tricking hdf5 initially, then failing on read
+    e = f.require_group("/BadFakeU235/energy")
+    e.create_dataset("294K", data=np.array([b"a", b"b", b"c"]))
+    x = f.require_group("/BadFakeU235/reactions/reaction_002/294K")
+    x.create_dataset("xs", data=np.array([b"a", b"b", b"c"]))
+
+out = Path("FakeU235.h5")
+with h5py.File(out, "w") as f:
+    e = f.require_group("/FakeU235/energy")
+    e.create_dataset("294K", data=np.array([0.0, 1.0, 2.0], dtype=np.float64))
+    x = f.require_group("/FakeU235/reactions/reaction_002/294K")
+    x.create_dataset("xs", data=np.array([1.0, 2.0, 3.0], dtype=np.float64))
