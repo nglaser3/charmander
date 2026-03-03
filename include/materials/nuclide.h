cb0b2c7936b4457f3c33c3004a2ad96c6433c7a7
Nathan Glaser <glaserna@oregonstate.edu>
2026-03-02 22:51:01 -0800

    setup xs interpolation on nuclide class

--- include/materials/nuclide.h
+++ include/materials/nuclide.h
@@ -2,10 +2,17 @@
 #define CHARMANDER_MATERIALS_NUCLIDE_H_
 
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 namespace charmander
 {
+enum MT {
+  ELASTIC=2,
+  INELASTIC=4,
+  FISSION=18,
+  CAPTURE=102,
+};
   
 class Nuclide
 {
@@ -14,7 +21,13 @@ class Nuclide
 
     void LoadFromFile();
 
-    void ConstructTotalXS(std::vector<float>& total_xs) const;
+    void ConstructTotalXS(std::vector<float> total_xs) const;
+
+    size_t GetLowerEnergyBin(double energy) const;
+
+    double GetTotalXS(size_t energy_index, double energy) const;
+
+    double GetXSFromMT(MT mt, size_t energy_index, double energy) const;
   private:
     std::string nuclide_name_;
     std::string temperature_ = "294K";
@@ -22,10 +35,7 @@ class Nuclide
     std::vector<double> evaluation_energies_;
 
     std::vector<float> total_xs_;
-    std::vector<float> elastic_xs_;
-    std::vector<float> inelastic_xs_;
-    std::vector<float> capture_xs_;
-    std::vector<float> fission_xs_;
+    std::unordered_map<MT, std::vector<float>> xs_map_;
 };
 
 } // namespace charmander
--- src/materials/nuclide.cc
+++ src/materials/nuclide.cc
@@ -1,3 +1,6 @@
+#include <algorithm>
+#include <iterator>
+
 #include "materials/xs_file_interface.h"
 #include "materials/nuclide.h"
 
@@ -14,19 +17,68 @@ void Nuclide::LoadFromFile() {
 
   // fill the xs
   total_xs_.assign(evaluation_energies_.size(), 0.0f); //temporary before adding an actual function
-  xs_file.Load1DXSDataset("002", temperature_, elastic_xs_, evaluation_energies_.size());
-  xs_file.LeftPadLoad1DXSDataset("004", temperature_, inelastic_xs_, evaluation_energies_.size());
-  xs_file.Load1DXSDataset("018", temperature_, fission_xs_, evaluation_energies_.size());
-  xs_file.Load1DXSDataset("102", temperature_, capture_xs_, evaluation_energies_.size());
+  xs_file.Load1DXSDataset("002", temperature_, xs_map_[MT::ELASTIC], evaluation_energies_.size());
+  xs_file.LeftPadLoad1DXSDataset("004", temperature_, xs_map_[MT::INELASTIC], evaluation_energies_.size());
+  xs_file.Load1DXSDataset("018", temperature_, xs_map_[MT::FISSION], evaluation_energies_.size());
+  xs_file.Load1DXSDataset("102", temperature_, xs_map_[MT::CAPTURE], evaluation_energies_.size());
 
   // calculate the total xs from the above
   ConstructTotalXS(total_xs_);
 };
 
-void Nuclide::ConstructTotalXS(std::vector<float>& total_xs) const {
+void Nuclide::ConstructTotalXS(std::vector<float> total_xs) const {
   size_t length = evaluation_energies_.size();
-  for (size_t i = 0; i < length; ++i) {
-    total_xs[i] = elastic_xs_[i] + inelastic_xs_[i] + fission_xs_[i] + capture_xs_[i];
+  for (const auto& [mt, xs] : xs_map_)
+  {
+    for (size_t i = 0; i < length; ++i) {
+      total_xs[i] = xs[i];
+    }
   }
+};
+
+size_t Nuclide::GetLowerEnergyBin(double energy) const {
+  const size_t size_of_energies = evaluation_energies_.size();
+  const double* energies = evaluation_energies_.data();
+
+  if (energy <=  energies[0]) return 0;
+  if (energy >= energies[size_of_energies-1]) return size_of_energies - 2;
+  
+
+  auto it = std::lower_bound(energies, energies + size_of_energies, energy);
+  return static_cast<size_t>(it - energies) - 1;
+}
+
+double Nuclide::GetTotalXS(const size_t energy_index, const double energy) const {
+
+  if (energy <= evaluation_energies_.front()) return total_xs_.front();
+  if (energy >= evaluation_energies_.back()) return total_xs_.back();
+
+  const double* energies = evaluation_energies_.data();
+  const float* xs = total_xs_.data();
+
+  double E_low = energies[energy_index];
+  double E_high = energies[energy_index+1];
+
+  double XS_low = xs[energy_index];
+  double XS_high = xs[energy_index+1];
+
+  return XS_low + (XS_high - XS_low) * (energy - E_low) / (E_high - E_low);
+}
+
+double Nuclide::GetXSFromMT(MT mt, const size_t energy_index, const double energy) const {
+
+  const float* xs = xs_map_.at(mt).data();
+  if (energy <= evaluation_energies_.front()) return xs[0];
+  if (energy >= evaluation_energies_.back()) return xs[evaluation_energies_.size() - 1];
+  
+  const double* energies = evaluation_energies_.data();
+
+  double E_low = energies[energy_index];
+  double E_high = energies[energy_index+1];
+
+  double XS_low = xs[energy_index];
+  double XS_high = xs[energy_index+1];
+
+  return XS_low + (XS_high - XS_low) * (energy - E_low) / (E_high - E_low);
 }
 } // namespace charmander
