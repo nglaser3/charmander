3ec2eee29792070cfea16249cc16c24d8539f6c5
Nathan Glaser <glaserna@oregonstate.edu>
2026-03-09 19:07:59 -0700

    add in dummy cross sections for 004, 018, and 102

--- tests/data/fake_cross_sections/make_fake.py
+++ tests/data/fake_cross_sections/make_fake.py
@@ -14,5 +14,7 @@ out = Path("FakeU235.h5")
 with h5py.File(out, "w") as f:
     e = f.require_group("/FakeU235/energy")
     e.create_dataset("294K", data=np.array([0.0, 1.0, 2.0], dtype=np.float64))
-    x = f.require_group("/FakeU235/reactions/reaction_002/294K")
-    x.create_dataset("xs", data=np.array([1.0, 2.0, 3.0], dtype=np.float64))
+    xs_data = np.array([1.0, 2.0, 3.0], dtype=np.float64)
+    dataset_str = "/FakeU235/reactions/reaction_{}/294K"
+    for mt in ["002", "004", "018", "102"]:
+        f.require_group(dataset_str.format(mt)).create_dataset("xs", data=xs_data)
