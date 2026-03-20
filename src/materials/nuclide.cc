9be2a9caa5dfad6579892750cbc07979b05db467
Nathan Glaser <glaserna@oregonstate.edu>
2026-03-19 23:46:01 -0700

    make collision sample constant

--- src/materials/nuclide.cc
+++ src/materials/nuclide.cc
@@ -97,7 +97,7 @@ double Nuclide::GetXSFromMT(MT mt, const size_t energy_index,
 MT Nuclide::SampleReaction(const size_t energy_index, const double energy, double random) const {
   double total_xs = GetTotalXS(energy_index, energy);
   if (total_xs <= 0.0) return MT::MISSED;
-  for (const auto& [mt_rxn, _] : xs_map_)
+  for (const auto& mt_rxn : {MT(2), MT(4), MT(18), MT(102)})
   {
     random -= (GetXSFromMT(mt_rxn, energy_index, energy) / total_xs);
     if (random <= 0.0) return mt_rxn;
