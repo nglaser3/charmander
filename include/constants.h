b76c3792095a7757b0bae1c1d9eced82e726f715
Nathan Glaser <glaserna@oregonstate.edu>
2026-03-20 13:56:54 -0700

    add avogadro and barn

--- include/constants.h
+++ include/constants.h
@@ -10,6 +10,11 @@ namespace charmander
   constexpr double FP_TOLERANCE = 1e-12;
 
   constexpr double COINCIDENT_SURF = 1e-12;
+
+  constexpr double BARN = 1e-24;
+  
+  constexpr double AVOGADRO = 6.022e23 * BARN;
+
 } // namespace charmander
 
 #endif  // CHARMANDER_CONSTANTS_H_
